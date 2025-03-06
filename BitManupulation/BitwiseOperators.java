59b4b9b64df090b423a56b3ea22fd016d64d796d
SARTHAK SHARMA <141639566+Sarthak92101@users.noreply.github.com>
2025-03-06 21:07:39 +0530

    Add files via upload

---
+++ BitManupulation/BitwiseOperators.java
@@ -0,0 +1,12 @@
+package BitManupulation;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        System.out.println(5&6);
+        System.out.println(5|6);
+        System.out.println(5^6);
+        System.out.println(5<<2);
+
+
+    }
+}
