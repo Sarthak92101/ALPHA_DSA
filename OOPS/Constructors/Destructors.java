665d0def6222b811d3cec80f00f3cf898504089b
SARTHAK SHARMA <141639566+Sarthak92101@users.noreply.github.com>
2025-03-18 20:22:40 +0530

    Delete OOPS directory

--- OOPS/Constructors/Destructors.java
+++
@@ -1,7 +0,0 @@
-package OOPS.Constructors;
-
-
-public class Destructors {
-  //we don't use Destructors in java because we have GARBAGE COLLECTORS in JAVA 
-  //Destructors are the functions which removes object memory 
-}
